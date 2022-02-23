4b560b66adf48674b1e401f38531cccd7054e045
AzadehDolatshahi <Azadeh_dolatshahi@yahoo.com>
2022-02-23 14:19:16 +0100

    init

---
+++ recommender.py
@@ -0,0 +1,23 @@
+"""
+contains various implementations for recommending movies
+"""
+
+import pandas as pd
+from utils import movies, ratings
+
+movies = pd.read_csv('./data/movies.csv')
+ratings = pd.read_csv('./data/ratings.csv')
+
+
+# recommender_systems_intro_filled.ipynb
+def recommend_random(query, ratings, k=10):
+    """
+    Recommends a list of k random movie ids
+    """
+    return [1, 20, 34, 25]
+
+
+if __name__=='__main__':
+    # list of liked movies
+    query = [1, 34, 56, 21]
+    print(recommend_random(query, movies))
